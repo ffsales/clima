dbcfc5ba7b461b886e993a53b3bf72dad40c04d8
unknown <felipe.ferreira.sales@gmail.com>
2019-02-06 06:59:11 -0200

    Commit inicial

---
+++ src/test/java/br/com/uol/clima/ClimaApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.uol.clima;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClimaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
